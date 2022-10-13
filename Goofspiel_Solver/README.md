1b2e01246ed452225d439d768e325680bf7fa377
Josiah Kiok <josiahkiok@Josiahs-MacBook-Pro.local>
2022-10-13 10:24:55 -0500

    Added goofspiel solver to the portfolio

---
+++ Goofspiel_Solver/README.md
@@ -0,0 +1,55 @@
+# Goofspiel Solver
+
+### Introduction to Goofspiel 
+
+Goofspiel, or the Game of Pure Strategy, is a psychological game. Each player has a limited hand of cards, and there is a central prize deck (cards worth certain values -- generally, 1 through some integer n inclusive). Players do not know the order of the cards in the prize deck. In each round, a card is flipped face-up from the prize deck, and players play a card from their hand face-down in an attempt to win it. Players flip their cards at the same time, and the player with a higher card takes the prize card (if there is a draw, nobody wins it), and this repeats until the prize deck is empty. The winner is the player with the highest sum of prize card values.
+
+This game has a lot of surprising depth: the obvious 'greedy' strategy (playing the nth highest card when the nth highest prize card is played) loses to a player who shifts the entire sequence by one (playing the n+1th highest card when the nth highest prize card is played, and playing 1 on the highest prize card). 
+
+In fact, every strategy loses to a player who plays n+1 every time their opponent plays n, with the exception of the highest card. 
+
+In practice, players do not enter the game with an exact permutation of cards they wish to play, and end up trying to read their opponent and either beat them by as little as possible, or lose by as much as possible (in order to save higher card values for later). 
+
+Since every strategy has a counter, there is no simple strategy that wins the most times. But this can be represented as a matrix game -- so it is possible to maximize the expected value of the difference between your score and your opponent's score with a little bit (an exaggeration) of dynamic programming.
+
+### The Solver
+
+One of the ORGs (Online Reality Games) I was in had me playing a combination of Gale and Goofspiel against another player. Gale was already mathematically solved. The host's variant of Goofspiel, having different prize card values other than the standard first n integers, was not. So I took it upon myself to code an entire solver from scratch. 
+
+It was too difficult to search through the solutions file during the actual game (even when translated onto Google Sheets) because automated tools, including CTRL-F, weren't allowed. But that's okay -- just the ideal probabilities for the modified initial state gave me such an advantage that it all but won me the game. (I then proceeded to blunder away my advantage, but my opponent wasn't able to capitalize on it, eventually leading to my win.)
+
+I have generalized the code and added additional comments to make things more readable. The prize cards can be adjusted as well as the individual player hands, but the number of prize cards should be equal to the number of cards each player has. This requires, among other things, the GUROBI optimizer, of which a temporary free version is available via "pip3 install gurobipy".
+
+Goofspiel for the first n integers up to n=13 has already been analyzed, and do note that my results for the initial state match up with the results on this site, confirming its validity: http://gcrhoads.byethost4.com/gops.html?i=1.
+
+Output for the 5-card case is currently in solution.txt, in the format
+player cards | opponent cards | prize cards | upcard | additive probability vector
+
+The additive probability vector is an easier way to use a probabiliy vector (say, in an actual game). It also does not have to deal with funny rounding issues and can be expressed compactly. Take the following example into account (which is near the bottom of solution.txt):
+
+1 2 3 4 5|1 2 3 4 5|1 2 3 4 5|4|1: 12 2: 19 3: 38 4: 59 5: 100 
+
+Both players have cards {1, 2, 3, 4, 5} and the deck has prize cards {1, 2, 3, 4, 5}. The current card that gets flipped up is a 4. 
+
+If you roll an 100-sided die, and get less than or equal to 12, play a 1.
+Otherwise, if you get less than or equal to 19, play a 2.
+Otherwise, if you get less than or equal to 38, play a 3.
+Otherwise, if you get less than or equal to 59, play a 4.
+Otherwise, play a 5.
+
+Using this throughout a game will maximize the expected value of your score difference against an optimal opponent.
+
+### Sample Output
+
+```python3 goofspiel_solver.py
+Restricted license - for non-production use only - expires 2023-10-25
+1 0.00017404556274414062
+2 0.9584460258483887
+3 1.6427948474884033
+4 0.36036086082458496
+5 0.004511833190917969
+1 [(0, 0.0), (1, 0.046988701493370366), (2, 0.8327260280411943), (3, 0.1202852704654353), (4, 0.0), (5, 0.0)]
+2 [(0, 0.0), (1, 0.18545955578064546), (2, 0.0), (3, 0.7374907173835069), (4, 0.07704972683584765), (5, 0.0)]
+3 [(0, 0.0), (1, 0.11816972506630222), (2, 0.11879904625111658), (3, 0.0), (4, 0.7630312286825812), (5, 0.0)]
+4 [(0, 0.0), (1, 0.12259051709006223), (2, 0.07346766284674612), (3, 0.19149836113740445), (4, 0.20433723414426616), (5, 0.4081062247815211)]
+5 [(0, 0.0), (1, 0.11230330163341617), (2, 0.024102683112364194), (3, 0.0), (4, 0.0), (5, 0.8635940152542196)]```
---
+++ Goofspiel_Solver/goofspiel_solver.py
@@ -0,0 +1,129 @@
+# Written by pseudonam
+# This program maximizes the expected value won from a Goofspiel game.
+# Algorithm modified from https://mdpi-res.com/games/games-03-00150/article_deploy/games-03-00150.pdf?version=1352361394
+
+# Various imports
+
+from itertools import combinations
+import pulp as p
+import time
+
+# These variables can be freely modified. Note that heavy GPU utilization will be required for hands larger than size 7.
+# score_list is the deck containing the prize cards, and your_hand and opponent_hand are self-explanatory
+
+score_list = (1,2,3,4,5)
+your_hand = (1,2,3,4,5)
+opponent_hand = (1,2,3,4,5)
+
+# Create a file to print results out to.
+
+fout = open('solution.txt', 'w')
+
+m = p.apis.GUROBI(msg=False)
+
+def computeStrategy(state, upcard, f, optimal, solver, file): # Computes a strategy given a given state and upcard.
+    # state is the (V, Y, P) triple -- that is, your cards, your opponent's cards, and the remaining deck cards. optimal is the dictionary of all optimal moves.
+    # the upcard is the current face-up prize card.
+    # also, this returns the expected score if played optimally. more importantly, this adds the optimal move vector into the dictionary named optimal.
+    lp_prob = p.LpProblem('problem', p.LpMaximize)
+    lp_dict = {} # maps integers to variables, which are used for the linear programming solution
+    lp_dict[0] = p.LpVariable("v")
+    for i in state[0]:
+        lp_dict[i] = (p.LpVariable(str(i), lowBound = 0))
+    lp_prob += lp_dict[0]
+    # adds each individual variable to the problem. they must all be >=0.
+    # add the various other conditions
+    pstate = list(state[2][:])
+    pstate.remove(upcard) # decides p in advance
+    pstate = tuple(pstate)
+    # this may seem a bit complicated, but it simply converts goofspiel into a matrix game based off of previous optimal play and feeds it to the solver.
+    # the following generates every possible player and opponent card combination. 
+    for i in range(len(state[1])):
+        summer = 0
+        y = list(state[1][:])
+        enemy_card = y.pop(i)
+        y = tuple(y)
+        for j in range(len(state[0])):
+            v = list(state[0][:])
+            player_card = v.pop(j)
+            v = tuple(v)
+            # the score of a new position is the score of the previous position plus or minus the upcard, depending on who wins.
+            f_constant = f[(v, y, pstate)]
+            if player_card > enemy_card:
+                f_constant += upcard
+            elif enemy_card > player_card:
+                f_constant -= upcard
+            summer += f_constant*lp_dict[state[0][j]] # the constant * x[i]
+        # the generic matrix game linear programming formula.
+        lp_prob += summer-lp_dict[0] >= 0
+    # note that we want a probability vector; a list n of probabilities detailing the card you should play
+    # the probabilities have to sum to 1, so add that to the LP.
+    summer = 0
+    for i in lp_dict:
+        if i != 0:
+            summer += lp_dict[i]
+    lp_prob += summer == 1
+    # now solve
+    status = lp_prob.solve(solver)
+    # now that the matrix is solved, the place the following in the dictionary which details optimal moves
+    optimal[(state, upcard)] = ([(i, p.value(lp_dict[i])) for i in lp_dict])
+    # place optimal moves in a special format in the output file
+    dict_to_string = ""
+    summer = 0
+    for i in lp_dict:
+        if i != 0:
+            summer += p.value(lp_dict[i])*100
+            dict_to_string += str(i) + ": " + str(int(summer)) + " "
+    fout.write(" ".join([str(x) for x in state[0]]) + "|" + " ".join([str(x) for x in state[1]]) + "|" + " ".join([str(x) for x in state[2]]) + "|" + str(upcard) + "|" + dict_to_string + "\n")
+    # and finally return the value of the given position
+    return p.value(lp_dict[0])
+
+# compute base cases
+f = {} # the expected score value for any triple (V, Y, P)
+optimal = {} # optimal moves for said triple yee
+
+# generates the expected point gain when one card remains for every set of cards and prize cards.
+
+t = time.time()
+
+for psub in your_hand:
+    for esub in opponent_hand:
+        for card in score_list:
+            if psub > esub:
+                # if your card wins, you score points equal to the prize card's value
+                f[((psub,), (esub,), (card,))] = card 
+            elif esub > psub:  
+                # if the opponent wins, they score points (equivalent to you losing points) equal to the prize card's value
+                f[((psub,), (esub,), (card,))] = card * -1
+            else: 
+                f[((psub,), (esub,), (card,))] = 0
+
+# compute cases recursively with larger number of cards remaining. also, prints the amount of time it takes to find n.
+print (1, time.time()-t)
+t = time.time()
+for i in range(2, len(score_list)+1):
+    for phand in combinations(your_hand, i):
+        for ehand in combinations(opponent_hand, i):
+            for cards in combinations(score_list, i):   
+                # by symmetry, if you swap game-states with your opponent, the value of the game for you should be negated.
+                # the following condition can be added, but solution.txt will lack around half its probability vectors.
+                # however, it is still usable by searching the position with hands switched.
+                """if (ehand, phand, cards) in f:
+                   f[(phand, ehand, cards)] = f[(ehand, phand, cards)]*-1
+                else:"""
+                value = 0 # the expected value of your hand if you play perfectly
+                # consider all possible upcards (a random one appears) and average the results to obtain the expected value
+                for card in cards:
+                    c = computeStrategy((phand, ehand, cards), card, f, optimal, m, fout)
+                    value += c
+                value = value / len(cards) # finish averaging the value by dividing
+                f[(phand, ehand, cards)] = value
+    # prints the processing time of each segment
+    print (i, time.time()-t)
+    t = time.time()
+
+# prints the probability vector for the game's opening indexed by the first upcard
+for i in score_list: 
+    print (i, optimal[((your_hand, opponent_hand, score_list), i)])
+
+fout.close()
